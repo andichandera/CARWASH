8105022cbbcc271ee2bbdb850b3539a707ff40cf
Andi Chandera <andi.chandera@gmail.com>
2018-11-25 14:18:56 +0700

    testing

--- CW.MAIN/FrnMain.cs
+++ CW.MAIN/FrnMain.cs
@@ -16,5 +16,10 @@ namespace CW.MAIN
         {
             InitializeComponent();
         }
+
+        private void changePasswordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
