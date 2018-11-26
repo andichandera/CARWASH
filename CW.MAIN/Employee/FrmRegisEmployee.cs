9b04214168425a6546bbf37e3a662f66a583e80a
Andi Chandera <andi.chandera@gmail.com>
2018-11-26 21:03:35 +0700

    commit

--- CW.MAIN/Employee/FrmRegisEmployee.cs
+++ CW.MAIN/Employee/FrmRegisEmployee.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CW.MAIN.Employee
+namespace CW.MAIN
 {
     public partial class FrmRegisEmployee : Form
     {
