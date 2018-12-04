e045cdeb3ab160c6955af88d6f39e0cd46e14396
Vincent2201 <Vincent2201@github.com>
2018-12-04 18:11:12 +0700

    commit

--- CW.MAIN/Employee/FrmRegisEmployee.Designer.cs
+++ CW.MAIN/Employee/FrmRegisEmployee.Designer.cs
@@ -241,7 +241,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(689, 392);
+            this.ClientSize = new System.Drawing.Size(682, 392);
             this.Controls.Add(this.RbFemale);
             this.Controls.Add(this.RbMale);
             this.Controls.Add(this.Employee);
